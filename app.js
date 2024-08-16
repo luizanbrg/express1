da0cb02686bf0886b010261598112bdcd6d0ec76
Luiza Nobrega <mmluiza_@hotmail.com>
2024-08-16 18:08:33 +0200

    installation express et config du serveur

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const app = express();
+
+app.use((req, res, next) => {
+  console.log("Requête reçue!");
+  next();
+});
+
+app.use((req, res, next) => {
+  res.status(201);
+  next();
+});
+
+app.use((req, res, next) => {
+  res.json({ message: "Votre requête bien été reçue!" });
+});
+
+module.exports = app;
