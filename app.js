6d221a98528c50b2092690f0c81fd156694ae9de
Luiza Nobrega <mmluiza_@hotmail.com>
2024-08-27 11:13:09 +0200

    env

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const path = require('path');
 
 mongoose
   .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .then(() => console.log('Connexion à MongoDB réussieee !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 const app = express();
